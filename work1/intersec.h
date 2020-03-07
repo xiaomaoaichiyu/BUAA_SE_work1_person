3b81f831585b933e41407dc1bef313604a95d06d
Clef <1844061236@qq.com>
2020-03-07 22:17:25 +0800

    struct Line

--- work1/intersec.h
+++ work1/intersec.h
@@ -9,8 +9,8 @@ class Point{
 	double x;
 	double y;
 public:
-	Point() {}
-	Point(double px, double py);
+	Point() { x = 0; y = 0; }
+	Point(double px, double py) { x = px; y = py; }
 	bool operator<(const Point& p) const {
 		if (this->x < p.x) {
 			return true;
@@ -26,5 +26,12 @@ public:
 		return this->x == p.x && this->y == p.y;
 	}
 
-	void setPoint(double px, double py);
+	void setPoint(double px, double py) { x = px; y = py; }
+};
+
+struct Line {
+	double x1, y1;
+	double x2, y2;
+public:
+	Line(double a1, double b1, double a2, double b2) { x1 = a1; y1 = b1; x2 = a2; y2 = b2; }
 };
