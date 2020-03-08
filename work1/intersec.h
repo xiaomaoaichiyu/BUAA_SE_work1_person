06377957793cfbc6f127afdeb875fe55e34df0e9
Clef <1844061236@qq.com>
2020-03-08 09:00:50 +0800

    point with vector

--- work1/intersec.h
+++ work1/intersec.h
@@ -25,8 +25,17 @@ public:
 	bool operator==(const Point& p) const {
 		return this->x == p.x && this->y == p.y;
 	}
-
 	void setPoint(double px, double py) { x = px; y = py; }
+	//vector operation
+	Point operator+(const Point& p) { return Point(x + p.x, y + p.y); }
+	Point operator-(const Point& p) { return Point(x - p.x, y - p.y); }
+	//return norm of [p-(0, 0)]
+	double norm() {
+		return x * x + y * y;
+	}
+	double outerPro(Point p) {
+		return 
+	}
 };
 
 struct Line {
@@ -34,6 +43,20 @@ struct Line {
 	double x2, y2;
 public:
 	Line(double a1, double b1, double a2, double b2) { x1 = a1; y1 = b1; x2 = a2; y2 = b2; }
+};
+
+
+class Circle {
+	double x;
+	double y;
+	double r;
+public:
+	Circle() { x = 0; y = 0; r = 0; }
+	Circle(double cx, double cy, double cr) { x = cx; y = cy; r = cr; }
+	bool IsIntersect(Line l);
+	bool IsIntersect(Circle c);
+	bool getIntersecWithLine(Line l);
+	bool getIntersecWithCircle(Circle c);
 };
 
 bool getCross(Line l1, Line l2, Point* res);
